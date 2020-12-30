3d573d4d0d1b9c319e334c569e882262c3ae4e0a
Waqar Hassan Khan <waqar.hassan866@gmail.com>
2020-12-30 16:53:55 +0600

    Create B. Summer Olympic.cpp

---
+++ B. Summer Olympic.cpp
@@ -0,0 +1,52 @@
+/***from dust i have come, dust i will be***/
+
+#include<bits/stdc++.h>
+
+typedef long long int ll;
+typedef unsigned long long int ull;
+
+#define dbg printf("in\n")
+#define nl printf("\n")
+#define N 1010
+#define inf 1e9
+
+#define sf(n) scanf("%d", &n)
+#define sff(n,m) scanf("%d%d",&n,&m)
+#define sfl(n) scanf("%I64d", &n)
+#define sffl(n,m) scanf("%I64d%I64d",&n,&m)
+
+#define pf(n) printf("%d",n)
+#define pff(n,m) printf("%d %d",n,m)
+#define pffl(n,m) printf("%I64d %I64d",n,m)
+#define pfl(n) printf("%I64d",n)
+#define pfs(s) printf("%s",s)
+
+#define pb push_back
+#define pp pair<int, int>
+
+using namespace std;
+
+int main()
+{
+    // freopen("in.txt", "r", stdin);
+
+    int i, j, k;
+    int n, m, t;
+
+    sf(t);
+    while(t--)
+    {
+        int y;
+        sf(y);
+
+        int ans = 1;
+        for(i = 2024; i <= y; i += 4)
+            ans++;
+
+        pf(ans);
+        nl;
+
+    }
+
+    return 0;
+}
